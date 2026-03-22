e97538ef3a133e3834412f5e9040010200a1bd97
Hendrik Rennau <hendrik.rennau@igp.fraunhofer.de>
2026-03-22 15:10:53 +0100

    add chats function

--- soniq/src/router/index.ts
+++ soniq/src/router/index.ts
@@ -8,6 +8,7 @@ import PlaylistDetail from '@/views/Playlist/PlaylistDetail.vue'
 import Community from '@/views/Community/Community.vue'
 import Friends from '@/views/Community/Friends.vue'
 import Groups from '@/views/Community/Groups.vue'
+import Chats from '@/views/Chats/Chats.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -56,6 +57,11 @@ const router = createRouter({
       name: 'groups',
       component: Groups,
     },
+    {
+      path: '/chats',
+      name: 'chats',
+      component: Chats,
+    }
   ],
 })
 
